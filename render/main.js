74f4573adcdd6707b58c192979902870657143a3
λ <Lyz@LyzdeMacBook-Pro.local>
2016-06-22 10:11:28 +0800

    20160623

--- render/main.js
+++ render/main.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import MainTreeView from '../components/main-tree-view.jsx'
 import MainAlertChart from '../components/main-alert-chart.jsx'
 import MainAlertList from '../components/main-alert-list.jsx'
+import MainTopology from '../components/main-topology.jsx'
 
 ReactDOM.render(<MainTreeView />, document.getElementById('J_LeftTree'));
 ReactDOM.render(<MainAlertChart />, document.getElementById('J_AlertChart'));
 ReactDOM.render(<MainAlertList />, document.getElementById('J_AlertList'));
+ReactDOM.render(<MainTopology />, document.getElementById('J_Topology'));
